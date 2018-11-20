40658da8cc95aadb5836a9a574862de0a4b6d0d6
lixiaozhen <jane_li@kq300061.com>
2018-11-20 15:49:00 +0800

    first

---
+++ index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{shop}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      shop: '商品'
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style>
+</style>
